2c5d74f3e6b36b8f4779a6a063ce4cc9c897695c
jacolpn <jacolpn@outlook.com>
2019-10-04 17:35:38 -0300

    Solucionado o problema do botao Deletar.

--- src/Api.js
+++ src/Api.js
@@ -7,11 +7,13 @@ const api = axios.create({
 export const carregarMetas = () => api.get('metas')
 export const carregarTipos = () => api.get('tipos')
 export const salvarMetas = (novaMeta) => api.post('metas', novaMeta)
+export const deletarMetas = (id) => api.delete('metas/' + id)
 
 const apis = {
     carregarMetas,
     carregarTipos,
-    salvarMetas
+    salvarMetas,
+    deletarMetas
 }
 
 export default apis
